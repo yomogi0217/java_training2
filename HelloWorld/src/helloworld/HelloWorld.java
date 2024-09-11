95498812f563ca00c9d8f5554858899dbda517f5
royro <royro@Yomogi-WinPC>
2024-09-11 18:36:47 +0900

    Hello World!

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld; 		
+		
+/**		
+ * Hello World クラス		
+ */		
+ public class HelloWorld {		
+	 /**	
+	 * main メソッド	
+	 * @param args 実行引数	
+	 */	
+	 public static void main(String[] args) {	
+		System.out.println("Hello World!");
+	}	
+}		
